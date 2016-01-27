02c2a0e9498097821291de1787e45da0eaad80e7
James Brennan <james@jamesbrennan.ca>
2016-01-27 10:45:38 -0800

    Add URL, description. Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject ring-aws-lambda-adapter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject ring-aws-lambda-adapter "0.1.0"
+  :description "A ring adapater for use with AWS Lambda and AWS API Gateway"
+  :url "https://github.com/jpb/ring-aws-lambda-adapter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
